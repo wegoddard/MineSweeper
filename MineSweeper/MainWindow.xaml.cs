52d0e6c0565b18286d0d87bdb5db84f901d5a69c
wegoddard <will.goddard@cadre5.com>
2014-10-17 10:47:42 -0400

    Add mine images, gridlines to gameboard background

--- MineSweeper/MainWindow.xaml.cs
+++ MineSweeper/MainWindow.xaml.cs
@@ -31,52 +31,56 @@ namespace MineSweeper
             int columns = 10;
             int rows = 10;
 
-            // Create grid
-            Grid GameBoard = new Grid();
-            GameBoard.Width = 400;
-            GameBoard.Height = 400;
-            GameBoard.HorizontalAlignment = HorizontalAlignment.Center;
-            GameBoard.VerticalAlignment = VerticalAlignment.Bottom;
-            GameBoard.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#C0C0C0"));
+            // Height and Width
+            gridBoard.Width = 160;
+            gridBoard.Height = 160;
 
             // Create  grid rows and columns
             for (int c = 0; c < columns; c++)
             {
                 ColumnDefinition col = new ColumnDefinition();
-                GameBoard.ColumnDefinitions.Add(col);
+                col.Width = new GridLength(16);
+                gridBoard.ColumnDefinitions.Add(col);
             }
 
             for (int r = 0; r < rows; r++)
             {
                 RowDefinition row = new RowDefinition();
-                GameBoard.RowDefinitions.Add(row);
+                row.Height = new GridLength(16);
+                gridBoard.RowDefinitions.Add(row);
             }
 
-            // Add gameboard to background grid
-            BackgroundGrid.Children.Add(GameBoard);
+
 
             // Create buttons
- 
             for (int c = 0; c < columns; c++)
             {
                 for (int r = 0; r < rows; r++)
                 {
+                    // Create image and add to first cell, overlayed by button
+                    Image image = new Image();
+                    image.Source = new BitmapImage(new Uri("assets/Mine1pxBorder.png", UriKind.Relative));
+                    image.Stretch = Stretch.None;
+                    Grid.SetRow(image, r);
+                    Grid.SetColumn(image, c);
+                    gridBoard.Children.Add(image);
+
                     Button button = new Button();
-                    button.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+                    button.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7B7B7B"));
                     button.BorderThickness = new Thickness(1);
-                    button.Height = 40;
-                    button.Height = 40;
+                    button.Height = 16;
+                    button.Height = 16;
+
                     // add click handler to button
                     button.Click += new RoutedEventHandler(button_Click);
 
                     // add button to grid
                     Grid.SetRow(button, r);
                     Grid.SetColumn(button, c);
-                    GameBoard.Children.Add(button);
-
-
+                    gridBoard.Children.Add(button);
                 }
             }
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
